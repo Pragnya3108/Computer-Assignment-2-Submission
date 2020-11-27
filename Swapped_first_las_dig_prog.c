83ae9144d3f8e1de44d58c2c4a43c5faeffca291
Pragnya3108 <75014631+Pragnya3108@users.noreply.github.com>
2020-11-27 18:51:31 +0530

    Add files via upload

---
+++ Swapped_first_las_dig_prog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main() 
+{
+    int n,count=0,lastdig,firstdig,i,t;
+    printf("\n Enter a number: ");
+    scanf("%d",&n);
+    lastdig=n%10;
+       while(n>=10)
+       {
+           n=n/10;
+           firstdig=n;
+       }
+       printf("\n The original first digit is: %d and the last digit is: %d",firstdig,lastdig);
+       for(i=0;i<n;i++)
+       {
+           t=firstdig;
+           firstdig=lastdig;
+           lastdig=t;
+       }
+        printf("\n The swapped first digit is: %d and the last digit is: %d",firstdig,lastdig);
+}

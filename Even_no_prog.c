f88a070cfda179321efa5b93db7b63649f4915d3
Pragnya3108 <75014631+Pragnya3108@users.noreply.github.com>
2020-11-27 18:26:59 +0530

    Add files via upload

---
+++ Even_no_prog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int i,n;
+     printf("\n Enter the final limit: ");
+      scanf("%d",&n);
+    printf("\n Even numbers from 1 to n are: ");
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("\n %d",i);
+        }
+    }
+}

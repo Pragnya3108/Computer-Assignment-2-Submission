f7fd0332aa7cd2c81f125ea0abd8072cb59a261c
Pragnya3108 <75014631+Pragnya3108@users.noreply.github.com>
2020-11-27 18:52:36 +0530

    Update and rename Even_no_prog.c to Odd_no_prog.c

--- Odd_no_prog.c
+++ Odd_no_prog.c
@@ -4,7 +4,7 @@ void main() {
     int i,n;
      printf("\n Enter the final limit: ");
       scanf("%d",&n);
-    printf("\n Even numbers from 1 to n are: ");
+    printf("\n Odd numbers from 1 to n are: ");
     for(i=1;i<=n;i++)
     {
         if(i%2!=0)
